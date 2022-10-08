4c424fc2f929a590168bda22898bc7ab80d19a84
uigitdev <ui.gitdev@gmail.com>
2022-10-08 13:38:33 +0200

    Fix pub.dev warnings

--- lib/src/stream_holder.dart
+++ lib/src/stream_holder.dart
@@ -4,16 +4,21 @@ class StreamHolder<T> {
   final _controller = StreamController<T>.broadcast();
   T _data;
 
+  /// Add default [StreamHolder] data, when you create it.
   StreamHolder(this._data) {
     addData(_data);
   }
 
+  /// Add stream value to the stream attribute of StreamBuilder.
   Stream<T> get stream => _controller.stream;
 
+  /// Manage [StreamHolder] with controller.
   StreamController<T> get controller => _controller;
 
+  /// Add data value to the stream attribute of StreamBuilder.
   T get data => _data;
 
+  /// Call it when you want to update the old [StreamHolder] data.
   void addData(T data) {
     _data = data;
     _controller.add(data);
